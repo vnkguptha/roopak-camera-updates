007e6b3e24f74a92aeb38a61f1c1967855a44039
vnkguptha <107602426+vnkguptha@users.noreply.github.com>
2026-06-02 06:30:32 +0000

    Add install.sh and update update.json

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cp -r dashboard/* /home/roopak/roopak-camera/dashboard/
+
+cp scripts/wifi_manager.py \
+/home/roopak/roopak-camera/
+
+cp scripts/camera_stream.sh \
+/home/roopak/roopak-camera/
+
+chmod +x \
+/home/roopak/roopak-camera/camera_stream.sh
+
+sudo systemctl restart camera.service
+sudo systemctl restart roopak-dashboard.service
+
+echo '{"version":"1.0.3"}' \
+> /home/roopak/roopak-camera/version.json
+
+sudo reboot
